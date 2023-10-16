97799d5f6a5bbe1fecdb0e7cc62ac9ec7c290a7e
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-16 14:31:48 -0400

    Update turnTracker.py

--- turnTracker.py
+++ turnTracker.py
@@ -1,39 +1,49 @@
 class Node:
-   def __init__(self, data):
+   def __init__(self, data = None):
       self.data = data
-      self.next = None
-      self.prev = None
+      self.next = self
+      self.prev = self
       
 class TurnTracker:
     def __init__(self):
-      self.head = None
-      self.tail = None
-      self.size = 0
+      self._head = None
+      self._tail = None
+      self._count = 0
       
     def addPlayer(self, player):
         new_node = Node(player)
-        current = self.Head
-        if self.size == 0 and not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
+        #print(new_node.data)
+        current = self._head
+        if self._count == 0 and not self._head and not self._tail:
+            self._head = new_node
+            self._tail = new_node
         else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        self.size += 1
+            self._tail.next = new_node
+            new_node.prev = self._tail
+            self._tail = new_node
+        self._count += 1
+        
+    def remove(self, data): ##this works as intended
+        cur = Node(data)
+        temp = self._head
+        #print(cur.data, temp.data, 'hey')
+        if cur == temp:
+            self.head = None
+        temp = self.head
+        #print(cur.data, temp.data, 'hi')
         
         
         
-        
-        
-    
-    def nextPlayer(self):
+    def nextPlayer(self): ##does not work yet
         #tList = self.L
-        temp = self.L[0]
-        if temp:
-            print(temp)
-            self.L.pop(0)
-            self.L.append(temp)
+        temp = self._head
+        if temp.next != None:
+            self.head = temp.next
+            self.remove(temp.data)
+            self.addPlayer(temp.data)
+            return temp.data
+        else:
+            return temp.prev
             
     def skipNextPlayer(self):
         #print(self.L)
@@ -65,28 +75,10 @@ class TurnTracker:
 
 
 tt = TurnTracker()
-tt.addPlayer('james')
-tt.addPlayer('mike')
-tt.addPlayer('bella')
-
-print('----')
-tt.nextPlayer()
-tt.nextPlayer()
-tt.nextPlayer()
-tt.nextPlayer()
-print('---')
-tt.skipNextPlayer()
-print('----------')
-tt.nextPlayer()
-tt.nextPlayer()
-tt.nextPlayer()
-tt.nextPlayer()
-print('---')
-tt.numberOfPlayers()
-print('---')
-print(tt.reverseTurnOrder())
-
-
-print('--')
-tt.printTT()
-
+tt.addPlayer(77)
+tt.addPlayer(45)
+tt.addPlayer(82)
+tt.addPlayer(10)
+print(tt.nextPlayer())
+print(tt.nextPlayer())
+print(tt.nextPlayer())
