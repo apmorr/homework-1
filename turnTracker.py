6bddace7f24cc52678d4faa645be240a25210a75
apmorr <144373529+apmorr@users.noreply.github.com>
2023-10-18 12:30:03 -0400

    Update turnTracker.py

--- turnTracker.py
+++ turnTracker.py
@@ -14,7 +14,7 @@ class TurnTracker:
         self._skipping = False
 
     def addPlayer(self, player):
-        """Adds a player to the turn tracker."""
+        """adds a player to turn tracker"""
         new_node = Node(player)
         if self._head is None:
             self._head = new_node
@@ -34,7 +34,7 @@ class TurnTracker:
         self._length += 1
         
     def nextPlayer(self):
-        """Returns the next player in the turn order based on the current game state."""
+        """returns next player"""
         if self._length == 0:
             raise RuntimeError("No players in the turn tracker.")
 
@@ -56,45 +56,35 @@ class TurnTracker:
         return current_player
 
     def numberOfPlayers(self):
-        """Returns the number of players in the turn tracker."""
+        """returns number of players"""
         return self._length
 
     def skipNextPlayer(self):
-        """Causes the next player in the turn order to be skipped."""
+        """causes next player to be skipped"""
         if not self._reversed:
             if not self._skipping:
-                # Skip one more player.
+                # Skip one
                 self._nextPlayer = self._nextPlayer.next
 
             else:
-                # Skip two more players.
+                # skip two
                 for _ in range(2):
                     self._nextPlayer = self._nextPlayer.next
 
         else:
             if not self._skipping:
-                # Skip one more player.
+                # Skip one
                 self._nextPlayer = self._nextPlayer.prev
 
             else:
-                # Skip two more players.
+                # Skip two
                 for _ in range(2):
                     self._nextPlayer = self._nextPlayer.prev
 
-        # Set skipping to True.
-        # If skipNextPlayer() is called again before nextPlayer(), it will skip two more players.
-        # If nextPlayer() is called after skipNextPlayer(), it will skip one more player.
-        # Either way, at least one player will be skipped.
-        # This behavior is consistent with how the Skip card works in Uno.
-        # If a player plays a Skip card, the next player in turn order is skipped.
-        # If two players in a row play Skip cards, the next two players in turn order are skipped.
-        # And so on...
-        # This behavior can be changed by modifying this method.
-        # For example, you could reset skipping to False at the end of this method to only skip one player per call to skipNextPlayer().
-        # Or you could add a parameter to this method to specify how many players to skip.
+        
         
     def reverseTurnOrder(self):
-        """Reverses the current direction of the turn order."""
+        """reverses direction"""
         self._nextPlayer = self._head
 
 
@@ -102,68 +92,3 @@ class TurnTracker:
               self._reversed = True
         else: 
             self._reversed = False
-# Example 1 usage 
-
-
-tt = TurnTracker()
-tt.addPlayer("Jake")
-tt.addPlayer("Lina")
-tt.addPlayer("Tim")
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-print(tt.numberOfPlayers())
-
- # Example 2
-print ("Example 2")
-tt= TurnTracker()
-tt.addPlayer("Jake")
-tt.addPlayer("Lina")
-tt.addPlayer("Tim")
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-tt.reverseTurnOrder() # Lina plays reverse card
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-
-
-
-#Example 3
-print ("Example 3")
-
-tt= TurnTracker()
-tt.addPlayer("Jake")
-tt.addPlayer("Lina")
-tt.addPlayer("Tim")
-print(tt.nextPlayer())
-print(tt.nextPlayer()) 
-print(tt.nextPlayer())
-tt.skipNextPlayer() # Tim plays Skip card
-print(tt.nextPlayer())
-tt.skipNextPlayer() # Lina plays Skip card
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-
-
-
-#Example 4
-print ("Example 4")
-
-tt =TurnTracker()
-tt.addPlayer("Jake")
-tt.addPlayer("Lina")
-tt.addPlayer("Tim")
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-tt.skipNextPlayer()# Tim plays Skip card
-print(tt.nextPlayer())
-tt.reverseTurnOrder # Lina plays Reverse card
-print(tt.nextPlayer())
-tt.skipNextPlayer() # Jake plays Skip card
-print(tt.nextPlayer())
-print(tt.nextPlayer())
-
-
